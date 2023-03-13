5cb6bbee914830240d377f6813168ad6a34df1ec
Sarah Rambaud <sarahrambaud9@gmail.com>
2023-03-13 15:53:36 +0100

    carPark_Unity

---
+++ Assets/dataFiles/Scripts/ParkingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingScript : MonoBehaviour {
+
+    [SerializeField]
+    Texture white, green;
+
+	
+	public void SetTextureWhite() {
+        GetComponent<Renderer>().material.mainTexture = white;
+	}
+
+    public void SetTextureGreen()
+    {
+        GetComponent<Renderer>().material.mainTexture = green;
+    }
+
+    private void Start()
+    {
+        SetTextureGreen();
+    }
+}
---
+++ Assets/dataFiles/Scripts/PlayerScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    [SerializeField]
+     private float cRotation = 35f, speed = 2f;
+
+    [SerializeField]
+    private GameObject avtg, avtd, arrd, arrg;
+
+    private bool canMoove=true;
+
+    private BoxCollider box;
+
+    public void Awake()
+    {
+    box: GetComponent<BoxCollider>();
+
+    }
+    // Update is called once per frame
+    void Update() {
+
+        if (!canMoove)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.LeftAlt))
+        {
+            Vector3 dir = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                dir = Vector3.forward;
+                avtg.transform.Rotate(Vector3.right * 200 * Time.deltaTime);
+                avtd.transform.Rotate(Vector3.right * 200 * Time.deltaTime);
+                arrg.transform.Rotate(Vector3.right * 200 * Time.deltaTime);
+                arrd.transform.Rotate(Vector3.right * 200 * Time.deltaTime);
+            }
+            else
+            {
+                dir = -Vector3.forward;
+                avtg.transform.Rotate(Vector3.right * -200 * Time.deltaTime);
+                avtd.transform.Rotate(Vector3.right * -200 * Time.deltaTime);
+                arrg.transform.Rotate(Vector3.right * -200 * Time.deltaTime);
+                arrd.transform.Rotate(Vector3.right * -200 * Time.deltaTime);
+            }
+            transform.Translate(dir * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * cRotation * Input.GetAxis("Horizontal") * Time.deltaTime);
+        }
+    }
+        
+        void OnCollisionEnter (Collision collision)
+        {
+            if (collision.gameObject.tag == "obstacles")
+            {
+                canMoove = false;
+                Debug.Log("Game Over !");
+            }
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            if (box.bounds.min.x > other.bounds.min.x && box.bounds.max.x < other.bounds.max.x
+                && box.bounds.min.y > other.bounds.min.y && box.bounds.max.y < other.bounds.max.y
+                && box.bounds.min.z > other.bounds.min.z && box.bounds.max.z < other.bounds.max.z)
+            {
+                GameObject.Find("parking").GetComponent<ParkingScript>().SetTextureGreen();
+            }
+            else
+            {
+                GameObject.Find("parking").GetComponent<ParkingScript>().SetTextureWhite();
+            }
+        }
+       
+        
+	}
+
